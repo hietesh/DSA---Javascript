d7ca1e58938b3d240bf915355bd2cb1bac73eae5
hietesh <imhietesh@gmail.com>
2023-05-02 14:50:44 -0700

    3 Basic Math Problems Done

--- src/Step 1/Step 1.4/basic-maths.js
+++ src/Step 1/Step 1.4/basic-maths.js
@@ -31,8 +31,8 @@ function checkPalindrome(n){
 
 // input for gcd
 
-const num1 = prompt('Num1: ');
-const num2 = prompt('Num2: ');
+// const num1 = prompt('Num1: ');
+// const num2 = prompt('Num2: ');
 
 function findGCD(num1,num2){
     let minNum = Math.min(num1,num2);
@@ -50,8 +50,40 @@ function findGCD(num1,num2){
 // gcd(a,b) = gcd(a-b,b) where a > b
 
 function findGCDEuclidean(num1,Num2){
-
+    
 }
 
-console.log(findGCD(3,4));
+function armStrongNumber(n){
+    const count = countDigits(n);
+    let x = Number(n);
+    let ans=0;
+    while(x!=0){
+        const powerValue = Math.pow(Math.floor(x%10),count);
+        ans+=powerValue;
+        x = Math.floor(x/10);
+    }
+    return ans == n;
+}
+
+function printAllDivisor(n){
+    let str="";
+    for(let i=1;i<=n;i++){
+        if(n%i==0){
+            str+=i+" ";
+        }
+    }
+    return str.trimEnd();
+}
+
+function checkPrime(n){
+    let x=Number(n);
+    for(let i=2;i< Math.sqrt(n);i++){
+        if(Math.floor(x%i)==0){
+            return false;
+        }
+    }
+    return true;
+}
+
+console.log(checkPrime(n));
 
