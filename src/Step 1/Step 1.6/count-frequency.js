c9d147dbebfb2bb6291d5379d890d2fdfd13e7b6
Hitesh Pagare <hitesh.pagare@uplers.in>
2023-05-05 03:27:43 +0530

    1 Hashing problem solved

---
+++ src/Step 1/Step 1.6/count-frequency.js
@@ -0,0 +1,44 @@
+const prompt = require('prompt-sync')();
+/*
+    Enter N: 6
+    10
+    5
+    10
+    15
+    10
+    5
+    10 = 3
+    5 = 2
+    15 = 1
+
+*/
+
+const n = prompt('Enter N: ');
+
+const arr = [];
+
+for(let i=0;i<n;i++){
+    arr[i]= prompt('');
+}
+
+function countFrequencies(arr){
+    const map = new Map();
+
+    // setting the freqencies in the map
+    for(let i=0;i<n;i++){
+        if(map.has(arr[i])){
+            map.set(arr[i],map.get(arr[i])+1);
+        }
+        else{
+            map.set(arr[i],1);
+        }
+    }
+
+    // showing the frequencies of the map
+    map.forEach( (value,key)=>{
+        console.log(key+ " = "+value);
+    });
+}
+
+
+countFrequencies(arr);
