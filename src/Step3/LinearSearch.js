30959e42458baa4fdb2a0483ff8e19edab7e7e89
Hitesh Pagare <imhietesh@gmail.com>
2023-05-13 22:37:59 +0530

    Union array half done

--- src/Step3/LinearSearch.js
+++ src/Step3/LinearSearch.js
@@ -15,12 +15,11 @@ function linearSearch(arr,num)
     const n = arr.length;
     for(let i=0;i<n;i++){
         if(arr[i]==num){
-            return i;
+            return i+1;
         }
-    }  
-
+    }
     return -1;
 }
 
 
-console.log(linearSearch(arr,n));
+console.log(linearSearch(arr,num));
---
+++ src/Step3/UnionArray.js
@@ -0,0 +1,24 @@
+const prompt = require('prompt-sync')();
+
+const n = prompt('Enter n: ');
+const m = prompt('Enter m: ');
+
+const arr1 = [];
+const arr2 = [];
+
+for(let i=0;i<n;i++){
+    arr1.push(prompt(`arr1[${i}] `));
+}
+
+for(let i=0;i<m;i++){
+    arr2.push(prompt(`arr2[${i}] `));
+}
+
+function unionSorted(arr1,arr2){
+    const union = [];
+    const i = 0;
+    const j = 0;
+
+     
+}
+
